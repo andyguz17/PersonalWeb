418c0db7b830dced0c2f4bd2b137eff9c9cad61e
Andres <andyguz17@gmail.com>
2021-09-18 13:17:46 -0400

    code formatting

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,27 @@ import { TranslateModule } from '@ngx-translate/core';
 import { ButtonComponent } from './button/button.component';
 import { HeaderComponent } from './header/header.component';
 import { UnderlineComponent } from './underline/underline.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [ButtonComponent, UnderlineComponent, HeaderComponent],
-  imports: [CommonModule, TranslateModule, RouterModule],
-  exports: [ButtonComponent, UnderlineComponent, HeaderComponent],
+  declarations: [
+    ButtonComponent,
+    UnderlineComponent,
+    HeaderComponent,
+    InputComponent,
+  ],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    RouterModule,
+    ReactiveFormsModule,
+  ],
+  exports: [
+    ButtonComponent,
+    UnderlineComponent,
+    HeaderComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule {}
