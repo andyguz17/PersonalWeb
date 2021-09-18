7893c20a0f6017f9823db976449304c80b1170ed
Andres <andyguz17@gmail.com>
2021-09-18 16:08:43 -0400

    Add login page

--- src/app/pages/login/login/login.component.ts
+++ src/app/pages/login/login/login.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  login: FormGroup;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  password = new FormControl('', Validators.required);
 
-  constructor() { }
+  error$ = this.translate.get('app.login.errors');
+  input$ = this.translate.get('app.login.inputs');
+  oauth$ = this.translate.get('app.login.oauth');
+  
+  constructor(private translate: TranslateService) {}
 
   ngOnInit(): void {
+    this.login = new FormGroup({
+      email: this.email,
+      password: this.password,
+    });
   }
 
+  submit() {
+    if (this.login.valid) {
+      console.log(this.login.value);
+    }
+  }
 }
--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.scss']
+  styleUrls: ['./input.component.scss'],
 })
-export class InputComponent implements OnInit {
+export class InputComponent {
+  @Input() control: FormControl;
+  @Input() type: string;
+  @Input() width: string;
+  @Input() placeholder: string;
 
-  constructor() { }
+  @Input() padding = '10px';
+  @Input() fontSize = '16px';
 
-  ngOnInit(): void {
+  styleInput(): Object {
+    return {
+      fontSize: this.fontSize,
+      padding: this.padding,
+      width: this.width,
+    };
   }
-
 }
